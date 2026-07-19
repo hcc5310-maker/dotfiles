af03f4dc5455c0d00749c2abee3a5cca5002e2b5
hcc5310-maker <hcc5310@gmail.com>
2026-07-19 21:43:42 +0800

    Initial dotfiles commit

---
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+﻿
+function cc { Set-Location 'G:\我的雲端硬碟\I_Claude_date'; $env:CLAUDE_CODE_NO_FLICKER=1; claude @args }
+
+
+function stock { $env:PYTHONIOENCODING='utf-8'; python 'G:\我的雲端硬碟\I_Claude_date\taiwan_stock_predictor\predictor.py' $args }
